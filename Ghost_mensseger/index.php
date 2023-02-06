c4da4c9f2a09e8afaa0b530d3227a9c4bd5467ec
Rick-s2 <114995830+Rick-s2@users.noreply.github.com>
2023-02-06 09:51:55 -0300

    Add files via upload

---
+++ Ghost_mensseger/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css?v=<?php echo time(); ?>">
+    <link rel="shortcut icon" href="\Ghost_mensseger\util\sol.ico" type="image/x-icon">
+    <title>Loguin Fantasma</title>
+</head>
+<body class = "index">
+    <div id ="cab">
+        <br>
+        <div id = "d_esquerda" class ="d_cab"> 
+        </div>
+       
+        <div id = "d_central" class ="d_cab">
+            <h1 id = "h1_cab"> Ghost messages </h1>
+        </div>
+
+        <div id = "d_direita" class ="d_cab">
+            <button type ="button" id = "login" class = "botoes">
+                <p class = "p_botao">
+          
+                <a  class = "link-as-button" href = "http://localhost/ghost_mensseger/login.php"> Login </a>
+
+                </p> 
+        
+            </button> 
+            <button type ="button" id = "Reg" class = "botoes"> 
+                <p class = "p_botao">
+                
+                <a  class = "link-as-button" href = "http://localhost/Ghost_mensseger/register.php">
+                
+                Registrar
+
+                </a>
+
+                </p> </button> 
+        </div>
+
+
+    </div>
+    <div id = "meio">
+
+        <br><br><br><br>
+
+        <div id = "explic">
+
+            <p id ="p_explic">
+            Um sistema de guardar recados, idéias e mensagens, para pessoas logadas.<br><br> E para pessoas não logadas?<br><br> É... apenas algumas telas bonitas <br>para vocês.</p> 
+
+        </div>
+    </div>
+    <div id = "rodape">
+        <div id= "insta">
+            
+        
+            <p id = "p_guille" class = "p_instagram"> Guillermo -  <a  class = "link-as-button" href = "https://www.instagram.com/gui_deltoro/?next=%2F">
+                                            @gui_deltoro 
+                                </a>  
+                        </p> 
+
+            <p id = "p_rick" class = "p_instagram"> Ricardo - <a class = "link-as-button" href = "https://www.instagram.com/ricardovisk_s2/?next=%2F">
+                                         @ricardovisk_s2
+                                </a>
+                        </p>
+
+        </div>
+     </div>
+</body>
+</html>
---
+++ Ghost_mensseger/login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style_log.css?v=<?php echo time(); ?>">
+    <link rel="shortcut icon" href="\Ghost_mensseger\util\sol.ico" type="image/x-icon">
+    <title>Loguin Fantasma</title>
+</head>
+<body class = "login">
+<div id ="cab_log">
+        <br>
+        <div id = "d_esquerda_log" class ="d_cab_log"> 
+       
+                    
+                <button id = "voltar_log"><a href="index.php" class = "link-as-button_log"> Voltar </a> </button>
+
+        </div>
+       
+        <div id = "d_central_log" class ="d_cab_log">
+        <a href = "http://localhost/ghost_mensseger/"><h1 id = "h1_cab_log"> Ghost messages </h1> </a>
+        </div>
+
+        <div id = "d_direita_log" class ="d_cab_log">
+            
+        </div>
+
+    </div>
+    <div id = "meio_log">
+
+        <br><br><br><br>
+
+        <div id = "form_log"><h1 id = "p_cadastro_log"><br> <br>
+            <form id="dentro_form_log">
+                <label class = "label_log">G-mail</label><br>
+                <input type = "text" placeholder = "G-mail" class= "dados_log" name = "gmail_log">
+                <br>
+                <label class = "label_log">Senha</label><br>
+                <input type = "password" placeholder = "Senha" class = "dados_log" name = "senha_log">
+                <br>
+                <button type ="button" id = "enviar_log"> <a  class = "link-as-button_log"> Enviar </a></button>
+                <br>
+            </form>
+        </div>
+    </div>
+    <div id = "rodape_log">
+        <div id= "insta_log">
+            
+        
+            <p id = "p_guille_log" class = "p_instagram_log"> Guillermo -  <a  class = "link-as-button" href = "https://www.instagram.com/gui_deltoro/?next=%2F">
+                                            @gui_deltoro 
+                                </a>  
+                        </p> 
+
+            <p id = "p_rick_log" class = "p_instagram_log"> Ricardo - <a class = "link-as-button" href = "https://www.instagram.com/ricardovisk_s2/?next=%2F">
+                                         @ricardovisk_s2
+                                </a>
+                        </p>
+
+        </div>
+</body>
+</html>
